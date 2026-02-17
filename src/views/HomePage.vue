681a8efbd40564f5160ebf4c0dbd693b816df753
beemtz0715-max <beemtz0715@gmail.com>
2026-02-17 16:50:28 +0000

    updated homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,6 +6,22 @@ const profile = ref({
   name: 'Bianca Martinez',
   slogan: 'Student at South Texas College',
   avatar: 'https://api.dicebear.com/9.x/fun-emoji/svg?seed=Valentina',
+  links: [
+    {
+      id: 1,
+      title: 'GitHub',
+      url: 'https://github.com/beemtz0715-max',
+      icon: 'code',
+      description: 'Check out my projects',
+    },
+    {
+      id: 2,
+      title: 'Linkedin',
+      url: 'https://www.linkedin.com/in/biancamartinez0715/',
+      icon: 'briefcase',
+      description: 'Connect with me professionally',
+    },
+  ],
 })
 </script>
 
@@ -22,17 +38,22 @@ const profile = ref({
           class="h-full w-full object-cover"
         />
       </div>
-      <h1 class="font-display mb-2 text-2xl font-bold text-white sm:text-3xl">
+      <h1 class="mb-2 font-display text-2xl font-bold text-white sm:text-3xl">
         {{ profile.name }}
       </h1>
       <p class="max-w-xs text-gray-300 sm:text-lg">{{ profile.slogan }}</p>
     </div>
 
     <!-- Link List -->
+    <!-- Link List -->
     <div class="flex w-full max-w-md flex-col gap-4">
       <LinkCard
-        v-for="x of [1, 2, 3]"
-        :key="x"
+        v-for="link in profile.links"
+        :key="link.id"
+        :title="link.title"
+        :url="link.url"
+        :description="link.description"
+        :icon="link.icon"
       />
     </div>
 
